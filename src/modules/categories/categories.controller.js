78fbb04fcd495dffade87dcff81ce486c4147a44
Sura Talalweh <surabasem20@gmail.com>
2023-11-01 00:26:02 +0300

    create singin , create signup

--- src/modules/categories/categories.controller.js
+++ src/modules/categories/categories.controller.js
@@ -75,14 +75,4 @@ export const updateCategory=async(req,res)=>{
 
 
 
-// export const createCategory = async (req,res) => {
-//  const name=req.body.name.toLowerCase();
-//  if(await categoryModel.findOne({name})){
-//     return res.status(409).json({message:"category name already exists"})
-//  }
-// const {secure_url,public_id}=await cloudinary.uploader.upload(req.file.path,{
-//     folder:`${process.env.APP_NAME}/categories`
-// })
-//  const cat= await categoryModel.create({name,slug:slugify(name),image:{secure_url,public_id}});
-//  return res.status(201).json({message:"success",cat});
-// }
+
